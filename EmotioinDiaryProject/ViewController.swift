be51592aa33be8f26f78a38984aaa9fa3b3eec5e
LDK <dlehdrl115@naver.com>
2022-07-12 19:55:06 +0900

    코드수정완료

--- EmotioinDiaryProject/ViewController.swift
+++ EmotioinDiaryProject/ViewController.swift
@@ -8,9 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    var cnt = 1
-    var clickCount : [Int] = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    
+    var emotionArr = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     
     @IBOutlet weak var btnLabel1: UILabel!
     @IBOutlet weak var btnLabel2: UILabel!
@@ -22,59 +21,41 @@ class ViewController: UIViewController {
     @IBOutlet weak var btnLabel8: UILabel!
     @IBOutlet weak var btnLabel9: UILabel!
     
-    var dic : [String:Int] = ["행복해" : 1, "사랑해" : 1]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    }
-    
-    
-    
-    @IBAction func Btn1Clicked(_ sender: Any) {
-        btnLabel1.text = "행복해 \(clickCount[0])"
-        clickCount[0] += 1
+        
+        
         
     }
     
-    @IBAction func Btn2Clicked(_ sender: Any) {
-        btnLabel2.text = "사랑해 \(clickCount[1])"
-        clickCount[1] += 1
-    }
-    
-    @IBAction func Btn3Clicked(_ sender: Any) {
-        btnLabel3.text = "좋아해 \(clickCount[2])"
-        clickCount[2] += 1
-    }
-    
-    @IBAction func Btn4Clicked(_ sender: Any) {
-        btnLabel4.text = "당황해 \(clickCount[3])"
-        clickCount[3] += 1
-    }
-    
-    @IBAction func Btn5Clicked(_ sender: Any) {
-        btnLabel5.text = "속상해 \(clickCount[4])"
-        clickCount[4] += 1
-    }
-    
-    @IBAction func Btn6Clicked(_ sender: Any) {
-        btnLabel6.text = "우울해 \(clickCount[5])"
-        clickCount[5] += 1
-    }
-    
-    @IBAction func Btn7Clicked(_ sender: Any) {
-        btnLabel7.text = "심심해 \(clickCount[6])"
-        clickCount[6] += 1
-    }
-    
-    @IBAction func Btn8Clicked(_ sender: Any) {
-        btnLabel8.text = "행복해 \(clickCount[7])"
-        clickCount[7] += 1
-    }
-    
-    @IBAction func Btn9Clicked(_ sender: Any) {
-        btnLabel9.text = "오랑해 \(clickCount[8])"
-        clickCount[8] += 1
+    @IBAction func BtnClicked(_ sender: UIButton) {
+        
+        emotionArr[sender.tag] += 1
+        let text = "\(sender.currentTitle!) \(emotionArr[sender.tag])"
+        
+        switch sender.tag {
+        case 0:
+            btnLabel1.text = text
+        case 1:
+            btnLabel2.text = text
+        case 2:
+            btnLabel3.text = text
+        case 3:
+            btnLabel4.text = text
+        case 4:
+            btnLabel5.text = text
+        case 5:
+            btnLabel6.text = text
+        case 6:
+            btnLabel7.text = text
+        case 7:
+            btnLabel8.text = text
+        case 8:
+            btnLabel9.text = text
+        default:
+            print("오류 발생")
+        }
     }
 }
 
